20c87fb0e9dda6d995c488e532ac25342bcd4691
pgegh <hovig@mail.com>
2021-12-16 00:32:54 +0100

    Add null option to the selector

--- src/views/devSelectorView.js
+++ src/views/devSelectorView.js
@@ -1,6 +1,7 @@
 export default function DevSelectorView({name, id, items}) {
     return (
         <select name={name} id={id}>
+            <option value={null}>None</option>
             {items.map(([key, value]) => <option key={key} value={key}>{value}</option>)}
         </select>
     )
