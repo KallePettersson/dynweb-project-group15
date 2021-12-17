6609287da3db3278bc8e29a95355bea5994d28c2
isachaglund <isac.haglund@hotmail.com>
2021-12-17 14:29:06 +0100

    remove santa, han var kortlivad

--- src/home/Header.js
+++ src/home/Header.js
@@ -8,21 +8,6 @@ function Header() {
       <h1 className="header-text-small">
         &nbsp; the visualization tool for sociology
       </h1>
-      <div className="christmas-gif">
-        <iframe
-          src="https://giphy.com/embed/qvbGIuX6Mkg8pP2lLT"
-          width="80"
-          height="80"
-          frameBorder="0"
-          class="giphy-embed"
-          allowFullScreen
-        ></iframe>
-        <p>
-          <a href="https://giphy.com/gifs/hypeenergydrinks-santa-hype-energy-qvbGIuX6Mkg8pP2lLT">
-            via GIPHY
-          </a>
-        </p>
-      </div>
     </div>
   );
 }
