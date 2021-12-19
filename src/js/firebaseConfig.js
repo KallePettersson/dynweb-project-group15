ccb259d604899f30cd5eb4d68b3e21d20fd72d47
Kalle Pettersson <kallepettersson98@hotmail.com>
2021-12-19 13:43:04 +0100

    Implemented firebase redundancy for criteria selected

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYrcgmP7qi6W8QOOJpGYDFOpJn1nQ_FS4",
+  authDomain: "geoloco-fd002.firebaseapp.com",
+  databaseURL: "https://geoloco-fd002-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "geoloco-fd002",
+  storageBucket: "geoloco-fd002.appspot.com",
+  messagingSenderId: "608268728868",
+  appId: "1:608268728868:web:1b90727d59a9b7af52f6b1"
+};
+
--- src/js/model/selectorReducer.js
+++ src/js/model/selectorReducer.js
@@ -1,12 +1,22 @@
+import { database } from "../firebaseModel"
+import { ref, onValue } from "firebase/database";
+
+
 const initialState = {
     country: "World",
     city: null,
     criteria: "health_care_index",
 }
 
+const starCountRef = ref(database, 'geoloco');
+onValue(starCountRef, (snapshot) => {
+    const data = snapshot.val();
+    initialState.criteria = data.criteria;
+});
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case"SELECT_COUNTRY":
+        case "SELECT_COUNTRY":
             return {
                 ...state,
                 country: action.payload.country
